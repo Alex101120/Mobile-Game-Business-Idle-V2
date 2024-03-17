07636e67a38486afb40fcf38a2783f144bb17c3a
Alex101120 <133406167+Alex101120@users.noreply.github.com>
2024-03-17 19:33:02 +0200

    V0.0.3

---
+++ Assets/Scripts/StatisticsScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StatisticsScript : MonoBehaviour
+{
+    public TMP_Text NetworthText;
+    public TMP_Text ClickStatText;
+    public TMP_Text MoneyStatText;
+    public ClickScript ClickScript;
+    public int ClickStat;
+    public long MoneyStat;
+    public long Networth;
+    void Start()
+    {
+        
+        
+    }
+    public void GetStat()
+    {
+        MoneyStat = ClickScript.moneyStatClick;
+        ClickStat = ClickScript.ClickStat;
+        Networth = MoneyStat;
+        ClickStatText.text=ClickStat.ToString();
+        MoneyStatText.text=FormatMoney(MoneyStat);
+        NetworthText.text = FormatMoney(Networth);
+    }
+    private string FormatMoney(long money)
+    {
+        string[] suffixes = { "", "K", "M", "B", "T", "Q", "Qi", "S", "Sp", "O", "N", "D" }; 
+        int suffixIndex = 0;
+
+        while (money >= 1000 && suffixIndex < suffixes.Length - 1)
+        {
+            money /= 1000;
+            suffixIndex++;
+        }
+
+        return money.ToString("F1") + suffixes[suffixIndex];
+    }
+    private void SaveGameStat()
+    {
+        
+        PlayerPrefs.SetString("NetWorth",Networth.ToString());
+    }
+    void Update()
+    {
+        
+    }
+}
